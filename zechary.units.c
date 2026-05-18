b70ab3fdf76b64f0d369991bfb352ba179c72ebe
zecharymukuna-gif <zecharymukuna@gmail.com>
2026-05-18 11:50:51 +0300

    Add files via upload

--- zechary.units.c
+++ zechary.units.c
@@ -1,1 +1,31 @@
+//programm to display units offered this semester
 
+/*
+
+description:programm to display units offered this semester
+name:zehary kamau
+Reg no:BDSA-01-0132/2026
+date:13/05/2026
+version 1
+*/
+
+#include <stdio.h>
+
+int main(){
+
+	
+
+	printf("units offered this year");
+	
+//display units offered this semester;
+
+    printf("structured programming\n");
+    printf("communication skills\n");
+    printf("health and wellness management\n");
+    printf("introduction to computer studies and internet\n");
+    printf("data ethics privacy and governance\n");
+    printf("basic mathematics\n");
+
+return 0; 
+
+}
