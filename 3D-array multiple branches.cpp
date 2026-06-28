865328c3e8f8bc11eff2957f8efefbbf0fbaef29
zecharymukuna-gif <zecharymukuna@gmail.com>
2026-06-28 22:41:44 +0300

    Add files via upload

---
+++ 3D-array multiple branches.cpp
@@ -0,0 +1,28 @@
+/*
+Description:3D-array, multiple branches
+Name:Zechary Kamau
+Date:27/06/2026
+Reg No:BDSA-01-0132/2026
+*/
+#include <stdio.h>
+int main(){
+	int chain[3][5][10];
+	int Totaloccupied;
+	int i,j,k;
+	int Occupiedrooms = 0;
+	
+	printf("Total number of occupied rooms\n");
+	for(i=0;i<3;i++){
+		for(j=0;j<5;j++){
+			for(k=0;k<10;k++){
+				printf("%d\n\t",chain[3][5][10]);
+			}
+		}
+		printf("\n");
+		Totaloccupied += chain[3][5][10];	
+	}
+	printf("\nThe Totaloccupied is %d",Totaloccupied);
+	    
+	return 0;
+}
+	
